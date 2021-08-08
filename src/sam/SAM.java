9f7555e5dde94acc0c853aa795550b19a66a6d93
AlexTprog <alexander.t1780@gmail.com>
2021-08-08 15:11:08 -0500

    Revert "agregando ventana principal"
    This reverts commit b50177722ec054794b74ff774978106f492b5b9a.

--- src/sam/SAM.java
+++ src/sam/SAM.java
@@ -1,15 +1,21 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package sam;
 
-import Controlador.ControladorMenu;
-import Vista.Vista_Menu;
-
+/**
+ *
+ * @author AlexTprog
+ */
 public class SAM {
 
+    /**
+     * @param args the command line arguments
+     */
     public static void main(String[] args) {
-        Vista_Menu vista = new Vista_Menu();
-        ControladorMenu abrir = new ControladorMenu(vista);
-        abrir.iniciar();
+        // TODO code application logic here
     }
 
 }
